4ba297badeb3055899599f9140fcb1947f36db7d
PrinceGarg23 <princegarg230@gmail.com>
2021-12-31 23:38:38 +0530

    new Project Pets Added

---
+++ PETS2/app/src/main/java/com/example/pets/data/PetContract.java
@@ -0,0 +1,65 @@
+package com.example.pets.data;
+
+import android.provider.BaseColumns;
+
+public final class PetContract {
+
+    // To prevent someone from accidentally instantiating the contract class,
+    // give it an empty constructor.
+    private PetContract() {}
+
+    /**
+     * Inner class that defines constant values for the pets database table.
+     * Each entry in the table represents a single pet.
+     */
+    public static final class PetEntry implements BaseColumns {
+
+        /** Name of database table for pets */
+        public final static String TABLE_NAME = "pets";
+
+        /**
+         * Unique ID number for the pet (only for use in the database table).
+         *
+         * Type: INTEGER
+         */
+        public final static String _ID = BaseColumns._ID;
+
+        /**
+         * Name of the pet.
+         *
+         * Type: TEXT
+         */
+        public final static String COLUMN_PET_NAME ="name";
+
+        /**
+         * Breed of the pet.
+         *
+         * Type: TEXT
+         */
+        public final static String COLUMN_PET_BREED = "breed";
+
+        /**
+         * Gender of the pet.
+         *
+         * The only possible values are {@link #GENDER_UNKNOWN}, {@link #GENDER_MALE},
+         * or {@link #GENDER_FEMALE}.
+         *
+         * Type: INTEGER
+         */
+        public final static String COLUMN_PET_GENDER = "gender";
+
+        /**
+         * Weight of the pet.
+         *
+         * Type: INTEGER
+         */
+        public final static String COLUMN_PET_WEIGHT = "weight";
+
+        /**
+         * Possible values for the gender of the pet.
+         */
+        public static final int GENDER_UNKNOWN = 0;
+        public static final int GENDER_MALE = 1;
+        public static final int GENDER_FEMALE = 2;
+    }
+}
