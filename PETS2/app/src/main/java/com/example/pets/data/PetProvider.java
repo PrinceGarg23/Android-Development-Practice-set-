d5983b49f96ee7f619f9671f0855b275add3fcc2
PrinceGarg23 <princegarg230@gmail.com>
2022-01-01 16:41:23 +0530

    Added content Provider

---
+++ PETS2/app/src/main/java/com/example/pets/data/PetProvider.java
@@ -0,0 +1,50 @@
+package com.example.pets.data;
+
+import android.content.ContentProvider;
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.net.Uri;
+
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+public class PetProvider extends ContentProvider {
+    public static final String LOG_TAG = PetProvider.class.getSimpleName();
+    private  PetDbHelper mDbHelper;
+
+    @Override
+    public boolean onCreate() {
+        mDbHelper = new PetDbHelper(getContext());
+        return true;
+    }
+
+    @Nullable
+    @Override
+    public Cursor query(@NonNull Uri uri, @Nullable String[] projection, @Nullable String selection, @Nullable String[] selectionArgs, @Nullable String sortOrder) {
+        return null;
+    }
+
+    @Nullable
+    @Override
+    public String getType(@NonNull Uri uri) {
+        return null;
+    }
+
+    @Nullable
+    @Override
+    public Uri insert(@NonNull Uri uri, @Nullable ContentValues values) {
+        return null;
+    }
+
+    @Override
+    public int delete(@NonNull Uri uri, @Nullable String selection, @Nullable String[] selectionArgs) {
+        return 0;
+    }
+
+    @Override
+    public int update(@NonNull Uri uri, @Nullable ContentValues values, @Nullable String selection, @Nullable String[] selectionArgs) {
+        return 0;
+    }
+}
